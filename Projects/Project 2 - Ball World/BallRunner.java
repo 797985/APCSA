3230c26d5bd1707c20b63c5b7173c047a4aee057
Nick Gonzales <42616190+797985@users.noreply.github.com>
2018-09-21 10:38:11 -0700

    Moved on to Activity 2

--- Projects/Project 2 - Ball World/BallRunner.java
+++ Projects/Project 2 - Ball World/BallRunner.java
@@ -11,8 +11,30 @@ public class BallRunner
         BallWorld ballWorld = new BallWorld(250, 250);
         TGPoint tgPoint = new TGPoint (0, 0);
         BallBot ballBot = new BallBot(ballWorld, tgPoint, 0, 10);
-        
-        
+        double x;
+
+        while (true){
+            if (ballBot.canMoveForward(ballWorld)){
+                ballBot.moveForward();
+            }else{
+                x = Math.random() *360;
+                ballBot.setHeading(x);
+
+            }
+
+        }
+
     }
-    
+
+    public int findFreeBallBotIndex(BallBot[] ballBotArray){
+        int ballBotArray[];
+        ballBotArray = new ballBotArray[10];
+        TGPoint entrancePoint = new TGPoint(0,0);
+        
+        
+        for (w = 0; w<ballBotArray.length; w++){
+            
+        }
+        return
+    }
 }
