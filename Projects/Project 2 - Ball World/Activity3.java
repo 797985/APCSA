151160fb1e011ca4e9f213cbad0b01ca94bc9789
Nick Gonzales <42616190+797985@users.noreply.github.com>
2018-10-01 10:20:35 -0700

    Finished activty 2, on activity 3

--- Projects/Project 2 - Ball World/Activity3.java
+++ Projects/Project 2 - Ball World/Activity3.java
@@ -5,7 +5,7 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Activity2
+public class Activity3
 {
     public static void run(){
         BallWorld ballWorld = new BallWorld(250, 250);
@@ -32,16 +32,33 @@ public class Activity2
         }
         return ballBotArray.length;
     }
-
-    public static void a2(){
-        Activity2 thisClass = new Activity2();
+    
+    public double distanceBetweenPoints(TGPoint point1, TGPoint point2){
+        
+        return Math.sqrt((Math.pow(point1.x-point2.x,2)) + (Math.pow(point1.y-point2.y,2)));
+        
+    }
+    public boolean entranceClear (BallBot[] ballBotArray, TGPoint entrancePoint){
+        BallRunner ballRunner = new BallRunner();
+        boolean tf = true;
+        for (int e = 0; e < ballBotArray.length; e++){
+            if (!(ballBotArray[e] == null)){
+                if (ballBot.getPoint() > 2 * ballBot.getRadius()){
+                    return false;
+                }else{
+                    return true;
+                }
+            }
+        }
+    }
+    public static void a3(){
+        Activity3 thisClass = new Activity3();
         int freeBallBotArray[];
         int freeBallBotIndex;
         BallBot[] ballBotArray = new BallBot[10];
         TGPoint findFreeBallBotIndex = new TGPoint();
         BallWorld ballWorld = new BallWorld(250, 250);
         TGPoint tgPoint = new TGPoint (0, 0);
-        
         double x = 0;
         BallBot currBallBot;
         while (true){
@@ -59,6 +76,7 @@ public class Activity2
                     }else {
                         x = Math.random() *360;
                         currBallBot.setHeading(x);
+                        
                     }
                 }
             }
