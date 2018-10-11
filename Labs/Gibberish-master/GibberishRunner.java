40c3ee738ed6d8250401af8b183c23cea05e68ce
Nick Gonzales <42616190+797985@users.noreply.github.com>
2018-10-11 10:35:50 -0700

    Gibberish

---
+++ Labs/Gibberish-master/GibberishRunner.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Runner here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class GibberishRunner
+{
+    public static void run(){
+        WordList5000 wordList5000 = new WordList5000();
+        Gibberish gibberish = new Gibberish();
+        System.out.println(gibberish);
+    }
+}
