21749b5a8d80cb0bf4024fa94c887c13cb5f2e6f
Nick Gonzales <42616190+797985@users.noreply.github.com>
2018-11-30 09:21:07 -0800

    couldn't figure out how to get getGreatestNeighbor to work

--- Labs/Lab1126 CellMatrix/CellMatrix.java
+++ Labs/Lab1126 CellMatrix/CellMatrix.java
@@ -7,17 +7,17 @@
  */
 public class CellMatrix{
     /**
-     * +++++++++++++++++++++Instance variables
+     * Couldn't figure out how to get the greatest neighbor
      */
-    
+
     private Cell[][] cells;
 
     /**
      * +++++++++++++++++++++Constructor for objects of class NumberMatrix
      */
     public CellMatrix(int a, int b){
-       cells = new Cell[a][b];
-       
+        cells = new Cell[a][b];
+
     }
 
     /**
@@ -25,52 +25,59 @@ public class CellMatrix{
      */
     //Print each row and column of nums to the console
     public void printMatrix(){
-       for(int r=0; r<cells.length; r++){
-           for(int c=0; c<cells[r].length; c++){
-               //nested for loop
-               System.out.print("\t" + cells[r][c].getInt());
+        for(int r=0; r<cells.length; r++){
+            for(int c=0; c<cells[r].length; c++){
+                //nested for loop
+                System.out.print("\t" + cells[r][c].getInt());
             }
             System.out.print("\n"); // \n = new line
         }
     }
     //  load nums with random with numbers between 1 and 10 inclusive
     public void loadMatrix(){
-       for(int r=0; r<cells.length; r++){
-           for(int c=0; c<cells[r].length; c++){
-               cells[r][c] = new Cell((int)(Math.random()*10)+1);
+        for(int r=0; r<cells.length; r++){
+            for(int c=0; c<cells[r].length; c++){
+                cells[r][c] = new Cell((int)(Math.random()*10)+1);
             }
-           
+
         }
     }
-     /**
+
+    /**
      * ++++++++++++++  methods
      */
-    
+
     //  Load neighbor cells into the neighbors array
     public void loadCellNeighbors(){
         for(int r=0; r<cells.length; r++){
-           for(int c=0; c<cells[r].length; c++){
-               if(r>0){ //get north
-                   cells[r][c].getNeighbors()[0]=cells[r-1][c];
+            for(int c=0; c<cells[r].length; c++){
+                if(r>0){ //get north
+                    cells[r][c].getNeighbors()[0]=cells[r-1][c];
                 }
-               if(c>0){ //get west
-                   cells[r][c].getNeighbors()[1]=cells[r][c-1];
+                if(c>0){ //get west
+                    cells[r][c].getNeighbors()[1]=cells[r][c-1];
                 }
-               if(c<cells[0].length){ //get east
-                   cells[r][c].getNeighbors()[2]=cells[r][c+1];
+                if(c<cells[0].length){ //get east
+                    cells[r][c].getNeighbors()[2]=cells[r][c+1];
                 }
-               if(r<cells.length-1){ //get south
-                   cells[r][c].getNeighbors()[3]=cells[r+1][c];
+                if(r<cells.length-1){ //get south
+                    cells[r][c].getNeighbors()[3]=cells[r+1][c];
                 }
             }
         } 
     }
     //  Finds the sum of each of the neighbors and returns the greatest 
     // neighbor value
+    //couldnt get getGreatestNeighbors to work
     public int getGreatestNeighbors(){
-        
-        return 0;
+        int sum = 0;
+        for (int i =0; i<cells.length; i++){
+            if(cells[i] != null){
+                sum += cells[i].length;
+            }
+        }
+        return sum;
+
     }
-   
-    
+
 }
